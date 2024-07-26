6a129a2de62e0cbc7be9d302274cde0eb08800c3
The DT <80153297+thedtvn@users.noreply.github.com>
2024-07-26 23:54:03 +0700

    Create middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,11 @@
+// src/headersMiddleware.js
+let fake_header = ["Express", "PHP", "ASP.NET", "Next.js", "JSF", "web2py", "Virgool", "Alpine", "Vanilla", "WP Engine", "PageFly", "ZendServer", "Elementor Cloud"];
+
+export async function onRequest({ request, next }) {
+  const response = await next();
+  
+  // Add custom headers
+  response.headers.set('X-Powered-By', fake_header);
+  
+  return response;
+}
