7a41e8d35f0818841c8a37ed7369dbe3d9f72da2
The DT <80153297+thedtvn@users.noreply.github.com>
2024-07-26 23:55:51 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,9 @@
 // src/headersMiddleware.js
-let fake_header = ["Express", "PHP", "ASP.NET", "Next.js", "JSF", "web2py", "Virgool", "Alpine", "Vanilla", "WP Engine", "PageFly", "ZendServer", "Elementor Cloud"];
 
 export async function onRequest({ request, next }) {
   const response = await next();
-  
+  let fake_header = ["Express", "PHP", "ASP.NET", "Next.js", "JSF", "web2py", "Virgool", "Alpine", "Vanilla", "WP Engine", "PageFly", "ZendServer", "Elementor Cloud"];
+
   // Add custom headers
   response.headers.set('X-Powered-By', fake_header);
   
